a76ffa15000069b90aa4ce1fc72ce8af1e06c424
diderot-sielinou <diderotsielinou@gmail.com>
2025-08-11 18:56:40 +0100

    Initialize project and resolve issues

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  testMatch: ['**/*.spec.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
+    },
+  },
+};
