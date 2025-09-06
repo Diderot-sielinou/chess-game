a6123268c83c1b405294714fe28dc5789078b5db
diderot-sielinou <diderotsielinou@gmail.com>
2025-09-06 08:19:24 +0100

    update to fix ai

--- src/modules/ai/llm.client.ts
+++ src/modules/ai/llm.client.ts
@@ -11,7 +11,7 @@ export class GeminiClient {
   ) {}
 
   private readonly apiUrl =
-    'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-preview-05-20:generateContent?key=';
+    'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=';
 
   private readonly maxRetries = 3;
   private readonly fallbackResponse = 'The AI is currently unavailable.';
